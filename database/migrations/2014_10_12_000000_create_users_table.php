e7b136c87bf0fb36bd9cc52fa894d995fa9ed51b
Mukan Rajapov <mukhan350192@gmail.com>
2021-01-25 19:03:57 +0600

    update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,21 +15,17 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('secondName');
-            $table->string('lastName')->nullable();
+            $table->string('fullName');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('status');
             $table->string('phone');
-            $table->string('birthDay');
-            $table->integer('city');
-            $table->string('address');
+            $table->string('password');
+            $table->integer('user_type');
+            $table->integer('city')->nullable();
+            $table->string('address')->nullable();
             $table->string('image')->nullable();
             $table->string('token')->nullable();
-            $table->integer('type');
-            $table->string('user_type');
+            $table->double('rating')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2021_01_25_125311_create_company_details_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompanyDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('company_details', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('types');
+            $table->bigInteger('bin')->nullable();
+            $table->string('registration')->nullable();
+            $table->string('license')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('company_details');
+    }
+}
