352b403562e6c3f08094f0250d46a330a2a9a387
Mukan Rajapov <mukhan350192@gmail.com>
2021-01-25 19:56:36 +0600

    update

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/key', function(){
+Route::get('/test', function(){
  //   Artisan::call('config:cache');
   //  Artisan::call('config:clear');
   //  Artisan::call('cache:clear');
