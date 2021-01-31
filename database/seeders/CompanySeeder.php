2e30739eedd0caf42982b01addd54fa734e3d0db
Mukan Rajapov <mukhan350192@gmail.com>
2021-01-31 22:11:55 +0600

    update

--- database/seeders/CompanySeeder.php
+++ database/seeders/CompanySeeder.php
@@ -6,7 +6,7 @@ use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class PostStatus extends Seeder
+class CompanySeeder extends Seeder
 {
     /**
      * Run the database seeds.
