4bfe5ca2407761622a88b5adf8175ec1fc45fc0c
Mukan Rajapov <mukhan350192@gmail.com>
2021-02-26 15:28:17 +0600

    update

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -19,13 +19,17 @@ class UserResource extends JsonResource
             'id' => $this->id,
             'fullName' => $this->fullName,
             'email' => $this->email,
-            'city' => $this->city,
             'phone' => $this->phone,
-            'address' => $this->address,
-            'image' => $this->image,
-            'token' => $this->token,
         ];
-
+        if ($this->city){
+            $array['city'] = $this->city;
+        }
+        if ($this->address){
+            $array['address'] = $this->address;
+        }
+        if (!$this->image){
+            $array['address'] = $this->image;
+        }
         if ($this->user_type == 2) {
             $array['companyDetails'] = CompanyResource::collection(DB::table('company_details')->where('user_id',$this->id)->get());
         }
