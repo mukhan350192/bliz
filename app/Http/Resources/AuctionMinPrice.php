bb8e1e036ac6ad451bf862f011b6e76c6d4e0b2a
Mukan Rajapov <mukhan350192@gmail.com>
2021-03-21 14:53:21 +0600

    update

--- app/Http/Resources/AuctionMinPrice.php
+++ app/Http/Resources/AuctionMinPrice.php
@@ -15,25 +15,18 @@ class AuctionMinPrice extends JsonResource
      */
     public function toArray($request)
     {
-        $price = DB::table('auction_orders')->select('id',DB::raw('MIN(price) as price'))->groupBy('id')->where('auction_id',$this->auction_id)->get();
-        var_dump($price);
+        $price = DB::table('auction_orders')->where('auction_id',$this->auction_id)->get();
+
         $count = DB::table('auction_orders')->where('auction_id',$this->auction_id)->count();
         $array = [];
 
         $price = json_decode($price);
-        $min = 0;
         if (isset($this->price)){
             foreach ($price as $p){
-                $min = $p->price;
                 $array['price'] = $p->price;
                 $currency = DB::table('currency')->select('name')->where('id',$p->currency)->first();
                 $array['currency'] = $currency->name;
-                if ($p->price < $min){
-                    $array['price'] = $p->price;
-                    $currency = DB::table('currency')->select('name')->where('id',$p->currency)->first();
-                    $array['currency'] = $currency->name;
-                }
-            }
+             }
             $array['count'] = $count;
         }
         return $array;
