a5fc78836857a63f9a3f22eefbfd7184c5c3970e
Mukan Rajapov <mukhan350192@gmail.com>
2021-03-21 23:59:26 +0600

    update

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1818,7 +1818,7 @@ class PostController extends Controller
                 break;
             }
 
-            $data = AuctionMinDetails::collection(DB::table('auction')->where('user_id',$user->id)->whereInNot('status',[5,6])->get());
+            $data = AuctionMinDetails::collection(DB::table('auction')->where('user_id',$user->id)->whereNotIn('status',[5,6])->get());
 
             $result['success'] = true;
             $result['data'] = $data;
