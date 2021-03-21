f4c5413bfc5dc5d8d9b25572d0855c9930419c0d
Mukan Rajapov <mukhan350192@gmail.com>
2021-03-21 13:39:20 +0600

    update

--- app/Http/Resources/PostAdditional.php
+++ app/Http/Resources/PostAdditional.php
@@ -10,45 +10,53 @@ class PostAdditional extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return array
      */
     public function toArray($request)
     {
         $array = [];
-        if (!empty($this->documents)){
-            $docs = explode(',',$this->documents);
+        if (!empty($this->documents)) {
+            $docs = explode(',', $this->documents);
             $docsArray = [];
-            foreach ($docs as $doc){
-                $s =DB::table('post_document')->where('id',$doc)->first();
-                $docsArray[] = $s->name;
+            foreach ($docs as $doc) {
+                $s = DB::table('post_document')->where('id', $doc)->first();
+                if (isset($s)) {
+                    $docsArray[] = $s->name;
+                }
             }
             $array['docs'] = $docsArray;
         }
-        if (!empty($this->loading)){
-            $docs = explode(',',$this->loading);
+        if (!empty($this->loading)) {
+            $docs = explode(',', $this->loading);
             $loading = [];
-            foreach ($docs as $doc){
-                $s =DB::table('post_loading')->where('id',$doc)->first();
-                $loading[] = $s->name;
+            foreach ($docs as $doc) {
+                $s = DB::table('post_loading')->where('id', $doc)->first();
+                if (isset($s)) {
+                    $loading[] = $s->name;
+                }
             }
             $array['loading'] = $loading;
         }
-        if (!empty($this->condition)){
-            $docs = explode(',',$this->condition);
+        if (!empty($this->condition)) {
+            $docs = explode(',', $this->condition);
             $docsArray = [];
-            foreach ($docs as $doc){
-                $s =DB::table('post_condition')->where('id',$doc)->first();
-                $docsArray[] = $s->name;
+            foreach ($docs as $doc) {
+                $s = DB::table('post_condition')->where('id', $doc)->first();
+                if (isset($s)){
+                    $docsArray[] = $s->name;
+                }
             }
             $array['condition'] = $docsArray;
         }
-        if (!empty($this->addition)){
-            $docs = explode(',',$this->addition);
+        if (!empty($this->addition)) {
+            $docs = explode(',', $this->addition);
             $docsArray = [];
-            foreach ($docs as $doc){
-                $s =DB::table('post_addition')->where('id',$doc)->first();
-                $docsArray[] = $s->name;
+            foreach ($docs as $doc) {
+                $s = DB::table('post_addition')->where('id', $doc)->first();
+                if(isset($s)){
+                    $docsArray[] = $s->name;
+                }
             }
             $array['addition'] = $docsArray;
         }
