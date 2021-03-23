7eeb4e0c6f4fda4efd840fa5b7f41b16bb135237
Mukan Rajapov <mukhan350192@gmail.com>
2021-03-24 00:34:02 +0600

    updaet

--- app/Http/Resources/UserForDetailOffer.php
+++ app/Http/Resources/UserForDetailOffer.php
@@ -11,31 +11,33 @@ class UserForDetailOffer extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return array
      */
     public function toArray($request)
     {
 
         $user = User::find($this->executor);
-        if ($user->user_type == 1){
+        if ($user->user_type == 1) {
             $array['price'] = $this->price;
             $array['currency'] = $this->currency;
             $array['fullName'] = $user->fullName;
             $array['type'] = 'Частное лицо';
             $array['user_id'] = $user->id;
+            $array['created_at'] = date('d.m.Y', strtotime($this->created_at));
         }
-        if ($user->user_type == 2){
+        if ($user->user_type == 2) {
             $data = DB::table('company_details')
-                ->join('company_types','company_details.types','=','company_types.id')
-                ->select('company_details.name','company_types.name as companyName')
-                ->where('company_details.user_id','=',$user->id)
+                ->join('company_types', 'company_details.types', '=', 'company_types.id')
+                ->select('company_details.name', 'company_types.name as companyName')
+                ->where('company_details.user_id', '=', $user->id)
                 ->get();;
             $array = [
                 'price' => $this->price,
                 'currency' => $this->currency,
-                'fullName' => $data[0]->companyName. ' '. $data[0]->name,
+                'fullName' => $data[0]->companyName . ' ' . $data[0]->name,
                 'user_id' => $user->id,
+                'created_at' => date('d.m.Y', strtotime($this->created_at)),
             ];
         }
 
