ead691aea2760d7f13b4fa1fffc0586d23407db5
mukhan350192 <mukhan350129@gmail.com>
2021-08-03 14:00:47 +0600

    update

--- app/Http/Controllers/SpecialEquipmentController.php
+++ app/Http/Controllers/SpecialEquipmentController.php
@@ -71,6 +71,9 @@ class SpecialEquipmentController extends Controller
         }
         $ids = implode(",",$ids);
         var_dump($ids);
+        $sql = "SELECT * FROM special_equipment WHERE id IN ($ids) LIMIT $take ORDER BY updated_at DESC";
+        $res = DB::select($sql);
+        var_dump($res);
         $ss = DB::table('special_equipment')->whereIn('id',[$ids])->skip($skip)->take($take)->orderByDesc('updated_at')->get();
         var_dump($ss);
         $data = EquipmentMin::collection(DB::table('special_equipment')->whereIn('id',[$ids])->skip($skip)->take($take)->orderByDesc('updated_at')->get());
