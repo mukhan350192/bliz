041888cb1dbe31df2caa0559d76ffef52d958fe8
mukhan350192 <mukhan350192@gmail.com>
2021-09-25 14:38:10 +0500

    update

--- app/Http/Resources/PostMinResource.php
+++ app/Http/Resources/PostMinResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\User;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Facades\DB;
 
@@ -19,6 +20,7 @@ class PostMinResource extends JsonResource
             'id' => $this->id,
             'updated_at' =>  $this->updated_at,
             'details' => DetailsMinResource::collection(DB::table('details')->where('post_id',$this->id)->get()),
+            'user' => UserResource::collection(User::where('id', $this->user_id)->get()),
         ];
         if (isset($this->priority) && $this->priority == 2){
             $array['top'] = true;
